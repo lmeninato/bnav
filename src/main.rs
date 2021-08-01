eaf215c8836ac50acea0867f50685b6b6e1eac6f
Lorenzo Meninato <>
2021-08-01 14:00:13 -0400

    Switch over to warp to use websockets

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,28 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use]
-extern crate rocket;
-
-use rocket::response::NamedFile;
-use std::env;
-use std::io;
-use std::path::{Path, PathBuf};
-// use rocket::response::content;
-use rocket_contrib::json::Json;
-
-// use crate::tile as tile;
 use bnav::board;
-use bnav::board::Board;
 use bnav::config as bnav_config;
 
-// use rocket::fairing::{Fairing, Info, Kind};
-// use rocket::http::Header;
-// use rocket::{Request, Response};
+use warp::Filter;
 
-// pub struct CORS;
+#[tokio::main]
+async fn main() {
 
-// impl Fairing for CORS {
-//     fn info(&self) -> Info {
-//         Info {
-//             name: "Add CORS headers to responses",
-//             kind: Kind::Response,
-//         }
-//     }
+    // GET /
+    let react_app = warp::fs::dir(bnav_config::FRONTEND_PATH)
+        .with(warp::log("test"));
 
-//     fn on_response(&self, _request: &Request, response: &mut Response) {
-//         response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-//         response.set_header(Header::new(
-//             "Access-Control-Allow-Methods",
-//             "POST, GET, PATCH, OPTIONS",
-//         ));
-//         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-//         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-//     }
-// }
+    // GET /api/board
+    let board = warp::path!("api" / "board")
+        .map(|| {
+            let board = board::read_board("boards/test.txt");
+            warp::reply::json(&board)
+        });
 
-#[get("/")]
-fn index() -> io::Result<NamedFile> {
-    files(Path::new("index.html").to_path_buf())
-}
+    let routes = react_app
+        .or(board);
 
-#[get("/<file..>")]
-fn files(file: PathBuf) -> io::Result<NamedFile> {
-    let page_directory_path = format!(
-        "{}/{}",
-        env!("CARGO_MANIFEST_DIR"),
-        bnav_config::FRONTEND_PATH
-    );
-    NamedFile::open(Path::new(&page_directory_path).join(file))
-}
-
-#[get("/api/board")]
-fn board() -> Json<Board> {
-    let board = board::read_board("boards/test.txt");
-    Json(board)
-}
-
-fn main() {
-    rocket::ignite()
-        .mount("/", routes![index, files, board])
-        .launch();
-}
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 8080))
+        .await;
+} 
